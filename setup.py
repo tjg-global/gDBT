7ef00839b0caf6693cda9f61538f800e9641cbb6
Tim Golden <tim.golden@global.com>
2022-07-11 09:11:24 +0100

    Bump version & add command line for dbt logger

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ import setuptools
 
 setuptools.setup(
     name='gdbt',
-    version='1.3.2',
+    version='1.3.3',
     description='Global DBT packages',
     author='Tim Golden',
     author_email='tim.golden@global.com',
     packages = ["gdbt"],
+    #~ install_requires = [
+        #~ "git+https://github.com/tjg-global/xlsxlib"
+    #~ ],
     entry_points = {
-        "console_scripts" : ["gdbt=gdbt.gdbt:command_line"]
+        "console_scripts" : [
+            "gdbt=gdbt.gdbt:command_line",
+            "gdbt-logger=gdbt.gdbt_logger:command_line"
+        ]
     }
 )
